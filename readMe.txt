863f5975ab2a042b03d7d837c8daed87208ba80e
Abraham Zilberblat <39259098+AZilberblat@users.noreply.github.com>
2020-12-22 16:31:32 +0200

    goarc home task water trapper

---
+++ readMe.txt
@@ -0,0 +1,4 @@
+Written by Abraham zilberblat
+Written in javaScript options to run it: vsCode with node.js or js comipler or in online idea or in webBrowser tools
+Solution in O(n) Complexity
+Took about 4-5 hours
---
+++ waterTrap.js
@@ -0,0 +1,51 @@
+const assert = require('assert');
+
+waterTrap = (heights) => {
+
+    // we need 3 elemnts minimum to trap water
+    if( heights.length < 3){
+        return 0
+    }
+
+    // Declering variabels
+    let maxRight = -1 
+    let maxLeft = -1 
+
+    let right = heights.length - 1 
+    let left = 0
+    
+    let waterTrapped = 0
+  
+    while (left <= right) {
+        //Geting the max wall height from both sides
+        maxLeft = heights[left] > maxLeft ? heights[left] : maxLeft
+        maxRight = heights[right] > maxRight? heights[right] : maxRight
+        
+        //Calculating the amount of water trapped in each iteration
+        if (maxLeft > maxRight) {
+            waterTrapped += maxRight - heights[right]
+            right--
+        }
+        else {
+            waterTrapped += maxLeft - heights[left]
+            left++
+        }
+    }
+  
+    return waterTrapped
+};
+
+// Expercted 6
+assert.strictEqual(waterTrap([0,1,0,2,1,0,1,3,2,1,2,1]),6)
+console.log("Given [0,1,0,2,1,0,1,3,2,1,2,1] output is " + waterTrap([0,1,0,2,1,0,1,3,2,1,2,1]))
+// Expercted 9
+assert.strictEqual(waterTrap( [4,2,0,3,2,5]),9)
+console.log("Given [4,2,0,3,2,5] output is " + waterTrap([4,2,0,3,2,5]))
+// Expected 0
+assert.strictEqual(waterTrap( [0,0,0,0]),0)
+console.log("Given [0,0,0,0] output is " + waterTrap([0,0,0,0]))
+assert.strictEqual(waterTrap( [0,1]),0)
+console.log("Given [0,1] output is " + waterTrap([0,1]))
+// assert should fail 
+assert.strictEqual(waterTrap( [0,1]),1)
+
